884d37149bf6ed53f10da0c57c2b4268220f18b8
Mihai Borbea <borbeamihai@gmail.com>
2021-08-20 00:52:28 +0300

    fix navigation after creation of layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
@@ -6,7 +7,7 @@ import { SidenavListComponent } from './sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [HeaderComponent, SidenavListComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, RouterModule],
   exports: [HeaderComponent, SidenavListComponent],
 })
 export class LayoutModule {}
