3766790d8dd9da930c5e68da7b51562d81e6fb8d
Mihai Borbea <borbeamihai@gmail.com>
2021-08-19 10:11:46 +0300

    small fix for auth-interceptor

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -23,7 +23,6 @@ export class AuthInterceptorService implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return this.store.pipe(
       select(selectAuthUser),
-      take(1),
       exhaustMap((user) => {
         if (!user) {
           return next.handle(req);
@@ -32,7 +31,8 @@ export class AuthInterceptorService implements HttpInterceptor {
           params: new HttpParams().set('auth', user.token),
         });
         return next.handle(modifiedReq);
-      })
+      }),
+      take(1)
     );
   }
 }
