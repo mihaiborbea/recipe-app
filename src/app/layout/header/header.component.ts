c7ed7cbce72d90c11f9fca36e52831f9e1ceb4cc
Mihai Borbea <borbeamihai@gmail.com>
2023-11-13 01:05:46 +0200

    feat: update app nav bar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,36 +1,23 @@
-import {
-  Component,
-  EventEmitter,
-  OnDestroy,
-  OnInit,
-  Output,
-} from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import * as AuthActions from '../../auth/state/auth.actions';
 import { selectAuthUser } from 'src/app/auth/state/auth.selectors';
 import { AppState } from 'src/app/core/state/app.store';
-import { User } from 'src/app/auth/domain/user.model';
+// import * as ShoppingListActions from 'src/app/shopping-list/state/shopping-list.actions';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnDestroy {
   @Output() public sidenavToggle = new EventEmitter();
   collapsed = false;
-  user: User = null;
-  private userSub: Subscription;
+  user$ = this.store.select(selectAuthUser);
+  private destroy$ = new Subject<void>();
 
   constructor(private store: Store<AppState>) {}
-
-  ngOnInit(): void {
-    this.userSub = this.store.select(selectAuthUser).subscribe((user) => {
-      this.user = user;
-      console.log('USER', this.user);
-    });
-  }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
@@ -41,6 +28,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.userSub.unsubscribe();
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 }
--- src/app/shopping-list/state/shopping-list.selectors.ts
+++ src/app/shopping-list/state/shopping-list.selectors.ts
@@ -8,6 +8,11 @@ const selectShpListState = createFeatureSelector<ShoppingListState>(
   SHOPPING_LIST_STATE_NAME
 );
 
+export const selectIngredientsCount = createSelector(
+  selectShpListState,
+  (state: ShoppingListState) => state.shoppingList?.ingredients.length
+);
+
 export const selectShoppingList = createSelector(
   selectShpListState,
   (state: ShoppingListState) => state.shoppingList
